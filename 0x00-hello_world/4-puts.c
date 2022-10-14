e2729a809d209beaca4e48ac224ebfc3a7b3dcfb
Philip Ochieng <ochiengphilip546@gmail.com>
2022-10-14 09:16:57 -0700

    mine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main() {
+int main(void) {
+
 	/* puts() displays the string inside quotation */
+	
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
+
 }
