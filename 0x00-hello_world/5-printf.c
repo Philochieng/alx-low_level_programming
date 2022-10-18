8a8ceeedcc596ca7daf8ee8cde15f6fb2543b76d
Philip Ochieng <ochiengphilip546@gmail.com>
2022-10-18 06:36:30 -0700

    my c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - displays the output of printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	/* prints with proper grammar, but the outcome is a piece of art, */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
