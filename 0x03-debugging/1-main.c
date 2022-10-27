0919257df196225dcfad50946cf4a4a40f80b831
Philochieng <114005499+Philochieng@users.noreply.github.com>
2022-10-28 02:23:00 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,6 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+	
 	/*
 	*while (1 < 10)
 	*{
@@ -19,6 +20,5 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
