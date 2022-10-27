1096be409219a7dbb296a9f5ee24b08ec246c238
Philip Ochieng <ochiengphilip546@gmail.com>
2022-10-27 05:48:02 -0700

    hello

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,18 @@
-#include "main.h"
+
+#include "main.h ZZ
 
 /**
  * _strlen - counts and returns string length
- * 
- * return: the length
+ * @s: the string
+ *
+ * Return the length
  */
 
 int _strlen(char *s)
 {
 	int counter = 0;
-
-	while (*s != 0)
-	{
-		counter++;
+	while (*s != 0)nter++;
 		s++;
 	}
-
-	return(counter);
+	return (counter);
 }
