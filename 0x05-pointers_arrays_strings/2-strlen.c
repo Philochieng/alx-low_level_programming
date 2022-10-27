dfa056eb91fc8ddb2f06257ac6ef12a89812de3e
Philip Ochieng <ochiengphilip546@gmail.com>
2022-10-27 02:28:33 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - counts and returns string length
+ * @s: the string
+ *
+ *Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter)
+}
