23c9d71b13ae872d2fe2723add99430078fde88b
Philip Ochieng <ochiengphilip546@gmail.com>
2022-11-10 07:06:11 -0800

    Phil

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,8 +3,7 @@
 
 /**
  * File: main.h
- * Desc: Header file with all function declarations 
- * 	 for this project's tasks.
+ * Desc: Header file with all function declaratiions for this project's tasks.
  */
 
 int _putchar(char c);
